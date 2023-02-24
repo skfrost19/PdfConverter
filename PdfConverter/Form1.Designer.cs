5d4853ede230d90b21539f1f0b5417af187d8d52
skfrost19 <saahhiill123456@gmail.com>
2023-02-24 22:03:04 +0530

    Add project files.

---
+++ PdfConverter/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PdfConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnSelectImages = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.labelSuccess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Nirmala UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(36, 284);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(149, 47);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnSelectImages
+            // 
+            this.btnSelectImages.Location = new System.Drawing.Point(129, 142);
+            this.btnSelectImages.Name = "btnSelectImages";
+            this.btnSelectImages.Size = new System.Drawing.Size(137, 36);
+            this.btnSelectImages.TabIndex = 2;
+            this.btnSelectImages.Text = "Select Image(s)";
+            this.btnSelectImages.UseVisualStyleBackColor = true;
+            this.btnSelectImages.Click += new System.EventHandler(this.btnSelectImages_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(216, 284);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(138, 47);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // labelSuccess
+            // 
+            this.labelSuccess.AutoSize = true;
+            this.labelSuccess.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.8F);
+            this.labelSuccess.Location = new System.Drawing.Point(103, 231);
+            this.labelSuccess.Name = "labelSuccess";
+            this.labelSuccess.Size = new System.Drawing.Size(0, 18);
+            this.labelSuccess.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(392, 451);
+            this.Controls.Add(this.labelSuccess);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSelectImages);
+            this.Controls.Add(this.btnGenerate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(410, 498);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image To PDF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnSelectImages;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label labelSuccess;
+    }
+}
+
---
+++ PdfConverter/Form1.cs
@@ -0,0 +1,79 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PdfConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnGenerate.Enabled = false;
+            btnSave.Enabled = false;
+        }
+
+        // creataing a list of images
+        List<string> images = new List<string>();
+        PdfDocument finalPdf = new PdfDocument();
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+            if (images.Count > 0)
+            {
+                foreach (string image in images)
+                {
+                    // create a page and resize the image to fit the page(do not crop the image)
+                    PdfPage page = document.AddPage();
+                    XGraphics gfx = XGraphics.FromPdfPage(page);
+                    XImage img = XImage.FromFile(image);
+                    gfx.DrawImage(img, 0, 0, page.Width, page.Height);
+                }
+            }
+            finalPdf = document;
+            labelSuccess.Text = "Pdf Generated Successfully!";
+            btnSave.Enabled = true;
+        }
+
+        private void btnSelectImages_Click(object sender, EventArgs e)
+        {
+            // creating a dialog box
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "jpg files (*.jpg)|*.jpg|*.png|*.JPG|*.JPEG|*.jpeg";
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] images = dialog.FileNames;
+                // add a popup window to alert the user for the success with images name .
+                MessageBox.Show("Image(s) selected:-\n " + string.Join("\n", images));
+                foreach (string image in images)
+                {
+                    // add images to the list
+                    this.images.Add(image);
+                }
+            }
+            labelSuccess.Text = images.Count + " images selected.";
+            btnGenerate.Enabled = true;
+
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // open a dialog box and ask the user for name and location
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "PDF Files (*.pdf)|*.pdf";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                finalPdf.Save(dialog.FileName);
+            }
+            // a popup window to alert the user for the success.
+            MessageBox.Show("Pdf Saved Successfully!");
+            labelSuccess.Text = "";
+            btnGenerate.Enabled = false;
+            btnSave.Enabled = false;
+        }
+    }
+}
